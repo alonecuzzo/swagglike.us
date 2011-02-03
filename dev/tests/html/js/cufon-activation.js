c83b32d7ebf965ad712df994782b48d826b6afd4
Jabari Bell <jabari.bell@pxlflu.net>
2011-02-02 20:35:52 -0500

    splash page named to index, more added to it

--- dev/tests/html/js/cufon-activation.js
+++ dev/tests/html/js/cufon-activation.js
@@ -3,3 +3,6 @@ Cufon.replace('h2');
 // for some reason not working for the login form
 // Cufon.replace('.login-form input');
 Cufon.replace('#right-panel');
+Cufon.replace('.splash-header');
+Cufon.replace('#splash-right-panel');
+
